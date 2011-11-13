1d1abf43b61663e6a9b7e7486caef967fd9409c0
Hussein Morsy <hussein@morsy.de>
2011-11-13 11:24:28 +0100

    step "Und ich meine Personalien eingebe" erfolgreich implementiert

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:flight_id])
+    @booking = Booking.new
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111112204656) do
+ActiveRecord::Schema.define(:version => 20111113100913) do
+
+  create_table "bookings", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "flight_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "flights", :force => true do |t|
     t.string   "nr"
